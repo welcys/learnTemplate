8b4f24820cb4a41a3070deb3664a6846ae373503
welch <welcys@github.com>
2023-08-03 14:33:14 +0800

    添加了student和school两张表

--- index/models.py
+++ index/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=20)
+    age = models.IntegerField()
+
+    favorite_sport = models.CharField(max_length=100, default='unknown')
+
+    def __str__(self):
+        return self.name
+
+
+class School(models.Model):
+    name = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
